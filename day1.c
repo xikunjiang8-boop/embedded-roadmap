cf0d613ebb5d57e39179042e68a8fc2ad8ae8652
xikunjiang8-boep <xikunjiang8@gmail.com>
2026-09-21 14:52:26 +0800

    Day 1: C语言基础-变量、sizeof与格式化输出

---
+++ day1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   
+   float v = 3.30f;
+   double d = 3.30;
+   
+   printf("Hello,TDLAS!\n");
+   printf("Name:Jkun\n");
+   printf("sizeof int = %zu\n",sizeof(int));
+   printf("sizeof float = %zu\n",sizeof(float));
+   printf("v = %.10f\n",v);
+   printf("d = %.10f\n",d);
+   
+	return 0;
+}
